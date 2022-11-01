405977ca056a469346038b30007ce4fb0a9a94e0
michaelipri <93557658+michaelipri@users.noreply.github.com>
2022-11-01 14:35:51 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
+    <Auth0Provider
+    domain="dev-csdoqr316rslzjur.us.auth0.com"
+    clientId="qa5yZrdcUMaZgofPdX6r3OJLlT8R5LDA"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
+   
 );
 
 // If you want to start measuring performance in your app, pass a function
