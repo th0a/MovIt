6538395863bac870a3dcf050646ef88bec6ccf79
michaelipri <93557658+michaelipri@users.noreply.github.com>
2022-11-04 09:18:45 -0400

    Update main.tf

--- deployment/main.tf
+++ deployment/main.tf
@@ -63,6 +63,13 @@ resource "aws_security_group" "taskMovIt_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+    ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
